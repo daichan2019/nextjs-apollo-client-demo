7e56ddbc70bfe9761e8da87b751ba118bc61830f
daichi_sato <daichiexperience@gmail.com>
2023-02-25 23:18:11 +0900

    add refetchqueries

--- src/features/user/hooks/use-create-user.ts
+++ src/features/user/hooks/use-create-user.ts
@@ -3,21 +3,24 @@ import { useMutation } from '@apollo/client';
 import { createUserDocument, getUsersDocument } from '@/graphql/queries/queries';
 
 export const useCreateUser = () => {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  const [createUser] = useMutation(createUserDocument, {
-    update(cache, { data }) {
-      const newUserFromResponse = data?.insert_users_one;
-      const existingUsers = cache.readQuery({ query: getUsersDocument });
+  // const [createUser] = useMutation(createUserDocument, {
+  //   update(cache, { data }) {
+  //     const newUserFromResponse = data?.insert_users_one;
+  //     const existingUsers = cache.readQuery({ query: getUsersDocument });
 
-      if (existingUsers && newUserFromResponse) {
-        cache.writeQuery({
-          query: getUsersDocument,
-          data: {
-            users: [newUserFromResponse, ...existingUsers.users],
-          },
-        });
-      }
-    },
+  //     if (existingUsers && newUserFromResponse) {
+  //       cache.writeQuery({
+  //         query: getUsersDocument,
+  //         data: {
+  //           users: [newUserFromResponse, ...existingUsers.users],
+  //         },
+  //       });
+  //     }
+  //   },
+  // });
+
+  const [createUser] = useMutation(createUserDocument, {
+    refetchQueries: [{ query: getUsersDocument }, 'GetUsers'],
   });
 
   return [createUser];
